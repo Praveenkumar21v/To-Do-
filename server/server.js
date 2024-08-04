e62d7ecc181afc2e76c4ad03b5233cc8d5f6865c
Praveenkumar21v <mr.chandu.22@gmail.com>
2024-08-04 15:26:15 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -10,8 +10,19 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://todofullstack13.netlify.app' 
+];
+
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
